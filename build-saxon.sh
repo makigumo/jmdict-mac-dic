219925d993613845d52743c795e97df8838e7374
Dan <dan@wadoku.de>
2017-08-17 16:38:18 +0200

    build-saxon.sh: try to download saxon automatically

--- build-saxon.sh
+++ build-saxon.sh
@@ -1,9 +1,30 @@
 #!/bin/sh -e
+SAXONURL=https://sourceforge.net/projects/saxon/files/Saxon-HE/9.8/SaxonHE9-8-0-4J.zip/download
 SAXON=saxon9he.jar
 XML=JMDict_e
 OUT=jmdict.xml
 XSL=jmdict2macos.xsl
 
-sed 's/ENTITY \([^ ]*\) ".*"/ENTITY \1 "\1"/' ${XML} > temp.xml
-java -DentityExpansionLimit=2000000 -Xmx1024M -jar ${SAXON} -s:temp.xml -opt:10 -t -xsl:${XSL} -o:${OUT}
-rm temp.xml
+if [ ! -e ${SAXON} ]; then
+    echo "[!] Saxon not found. Trying to download."
+    SAXON_ZIP=$(mktemp)
+    wget -q ${SAXONURL} -O ${SAXON_ZIP}
+    echo "[ ] Extracting."
+    unzip ${SAXON_ZIP} ${SAXON}
+    rm ${SAXON_ZIP}
+    echo "[+] Saxon is ready to use."
+fi
+
+TMP_XML=$(mktemp)
+
+echo "[ ] Pruning entities."
+sed 's/ENTITY \([^ ]*\) ".*"/ENTITY \1 "\1"/' ${XML} > ${TMP_XML}
+echo "[+] Pruning entities...done"
+
+echo "[ ] Transforming."
+java -DentityExpansionLimit=2000000 -Xmx1024M -jar ${SAXON} -s:${TMP_XML} -opt:10 -t -xsl:${XSL} -o:${OUT}
+echo "[+] Transforming...done"
+
+echo "[ ] Cleaning up."
+rm ${TMP_XML}
+echo "[+] Cleaning up...done"
