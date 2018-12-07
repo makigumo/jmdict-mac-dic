3ef896ae6e756410aeb1e236fd04cfc460549939
Dan <dan@wadoku.de>
2018-12-07 10:00:52 +0100

    use curl

--- build-saxon.sh
+++ build-saxon.sh
@@ -5,13 +5,13 @@ XML=JMDict_e
 OUT=jmdict.xml
 XSL=jmdict2macos.xsl
 
-if [[ ! -e ${SAXON} ]]; then
+if [[ ! -e "${SAXON}" ]]; then
     echo "[!] Saxon not found. Trying to download."
     SAXON_ZIP=$(mktemp)
-    if wget -q ${SAXONURL} -O ${SAXON_ZIP} > /dev/null; then
+    if curl -s -L -o "${SAXON_ZIP}" "${SAXONURL}" > /dev/null; then
         echo "[ ] Extracting."
-        unzip ${SAXON_ZIP} ${SAXON}
-        rm ${SAXON_ZIP}
+        unzip "${SAXON_ZIP}" "${SAXON}"
+        rm "${SAXON_ZIP}"
         echo "[+] Saxon is ready to use."
     else
     echo "[-] Saxon download failed."
@@ -21,13 +21,13 @@ fi
 TMP_XML=$(mktemp)
 
 echo "[ ] Pruning entities."
-sed 's/ENTITY \([^ ]*\) ".*"/ENTITY \1 "\1"/' ${XML} > ${TMP_XML}
+sed 's/ENTITY \([^ ]*\) ".*"/ENTITY \1 "\1"/' "${XML}" > "${TMP_XML}"
 echo "[+] Pruning entities...done"
 
 echo "[ ] Transforming."
-java -DentityExpansionLimit=2000000 -Xmx1024M -jar ${SAXON} -s:${TMP_XML} -opt:10 -t -xsl:${XSL} -o:${OUT}
+java -DentityExpansionLimit=2000000 -Xmx1024M -jar "${SAXON}" -s:"${TMP_XML}" -opt:10 -t -xsl:"${XSL}" -o:"${OUT}"
 echo "[+] Transforming...done"
 
 echo "[ ] Cleaning up."
-rm ${TMP_XML}
+rm "${TMP_XML}"
 echo "[+] Cleaning up...done"
