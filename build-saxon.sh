17799751a26e997df995a60b778867d84f3aaa58
Dan <dan@wadoku.de>
2020-05-16 16:13:20 +0200

    saxon-he 10.1

--- build-saxon.sh
+++ build-saxon.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
-SAXONURL=https://sourceforge.net/projects/saxon/files/Saxon-HE/10/Java/SaxonHE10-0J.zip/download
-SAXON=saxon-he-10.0.jar
+SAXONURL=https://sourceforge.net/projects/saxon/files/Saxon-HE/10/Java/SaxonHE10-1J.zip/download
+SAXON=saxon-he-10.1.jar
 XML=JMDict_e
 OUT=jmdict.xml
 XSL=jmdict2macos.xsl
